d7b6a6246b8ca2084f226d00cee401134077bb2d
Mohamed-Elbehery <dev.elbehery@gmail.com>
2024-05-15 23:42:24 +0300

    finished

--- models/orders.model.js
+++ models/orders.model.js
@@ -1,5 +1,24 @@
 const mongoose = require("mongoose");
 
+const OrderItemsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  unitprice: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  linetotal: {
+    type: Number,
+    required: true,
+  },
+});
+
 const OrderSchema = new mongoose.Schema(
   {
     orderTitle: {
@@ -9,6 +28,49 @@ const OrderSchema = new mongoose.Schema(
     orderTotalPrice: {
       type: Number,
       required: true,
+    },
+    orderId: {
+      type: String,
+    },
+    orderItems: {
+      type: [OrderItemsSchema],
+    },
+    customer: {
+      firstName: {
+        type: String,
+      },
+      lastName: {
+        type: String,
+      },
+      email: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+    },
+    deliveryAddress: {
+      name: {
+        type: String,
+      },
+      address1: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      zip: {
+        type: String,
+      },
+      country: {
+        type: String,
+      },
+      set: {
+        type: Boolean,
+      },
     },
   },
   { timestamps: true }
