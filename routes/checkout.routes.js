2c6ad95a2c9e2b7e4e4d5cc2993aa25215ea1c19
Gigalord <dev.elbehery@gmail.com>
2024-10-29 22:51:50 +0300

    done

--- routes/checkout.routes.js
+++ routes/checkout.routes.js
@@ -3,6 +3,28 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const express = require("express");
 const router = express.Router();
 
+export function calculateTotalPriceWithTax(cartTotalPrice) {
+  // Define the tax rate
+  const taxRate = 1.5 / 100; // 1.5%
+
+  // Calculate the subtotal (cart total + shipping)
+  const subtotal = cartTotalPrice;
+
+  // Calculate the tax amount
+  let taxAmount = subtotal * taxRate;
+
+  // If the tax amount is less than $1, set taxAmount to $1
+  if (taxAmount < 1) {
+    taxAmount = 1;
+  }
+
+  // Calculate the total price with the adjusted tax amount
+  const totalPrice = subtotal + taxAmount;
+
+  // Return the total price
+  return totalPrice;
+}
+
 router.post("/create-checkout-session", async (req, res) => {
   const line_items = req.body.cartItems?.map((book) => {
     return {
@@ -22,6 +44,13 @@ router.post("/create-checkout-session", async (req, res) => {
     };
   });
 
+  const taxes = calculateTotalPriceWithTax(
+    req.body.cartItems.reduce(
+      (total, book) => +total + +book.price * book.quantity,
+      0
+    )
+  );
+
   const session = await stripe.checkout.sessions.create({
     shipping_address_collection: {
       allowed_countries: ["AE", "EG", "BH", "OM", "SA"],
@@ -31,7 +60,7 @@ router.post("/create-checkout-session", async (req, res) => {
         shipping_rate_data: {
           type: "fixed_amount",
           fixed_amount: {
-            amount: 10 * 100,
+            amount: 10 * 100 + taxes * 100,
             currency: "aed",
           },
           display_name: "Shipping",
